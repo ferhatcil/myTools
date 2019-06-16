893fd6012084843b4465d69c0479c606678a15f2
ferhatcil <44881390+ferhatcil@users.noreply.github.com>
2019-06-16 15:02:06 +0300

    Update agentPS.ps1

--- agentPS.ps1
+++ agentPS.ps1
@@ -3,9 +3,9 @@
 # BITS, istemci ve sunucu arasında dosyaları aktaran bir hizmettir
 # Arka planda msiexec ile gizlice dosya indirmek
 Import-Module BitsTransfer
-Start-BitsTransfer -Source "https://download.eset.com/com/eset/apps/business/era/agent/latest/agent_x64.msi" -Destination "C:\eset.msi"
-$control = Test-Path -Path "C:\eset.msi"
+Start-BitsTransfer -Source "https://download.test.com/file.msi" -Destination "C:\file-name.msi"
+$control = Test-Path -Path "C:\file-name.msi"
 while($control){
-Start-Process msiexec.exe -Wait -ArgumentList '/I C:\eset.msi /quiet /norestart /l C:\installlog.txt'
+Start-Process msiexec.exe -Wait -ArgumentList '/I C:\file-name.msi /quiet /norestart /l C:\installlog.txt'
 $control = $False
 }
